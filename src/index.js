b0e05773020b5ed5dab7823c98fcfb5daeda09fc
jpdrsanchez <joaopedrosanchez302@gmail.com>
2021-11-30 17:50:17 -0300

    feat(index): configure chakra provider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
@@ -6,7 +7,9 @@ import App from './App'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
