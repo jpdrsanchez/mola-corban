b050a7ed91549c11fb4cbb2a0903a433357a8277
jpdrsanchez <joaopedrosanchez302@gmail.com>
2021-11-30 16:37:53 -0300

    feat: add react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  return <div></div>
+  return (
+    <Routes>
+      <Route path="/" element={<p>Home</p>} />
+      <Route path="/:id" element={<p>ID</p>} />
+    </Routes>
+  )
 }
 
 export default App
